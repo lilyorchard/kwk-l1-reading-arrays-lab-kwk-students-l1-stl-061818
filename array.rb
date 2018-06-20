a7c815367422b82c88beac282215e3b8ba1363a9
lilyorchard <lily@orchardfamily.com>
2018-06-20 16:09:41 +0000

    automatically backed up by learn

---
+++ array.rb
@@ -0,0 +1,19 @@
+# student_one = "Caroline"
+# student_two = "Millie"
+# student_three = "Sam"
+# student_four = "Sanoli"
+
+# # puts "In this class, we have a student named #{student_four}."
+
+# student_list = ["Caroline","Millie","Sam","Sanoli"]
+# puts student_list
+
+# friends = ["Monica", "Rachel", "Phoebe", "Ross", "Chandler", "Joey"]
+# puts friends
+
+disney_characters = ["Nala", "Ariel", "Cinderella", "Stitch", "Anna"]
+ages = [13,16,14]
+books = ["Fish Out of Water", "Purple Hibiscus", "Princess X"]
+# puts disney_characters, ages, books
+disney_characters.push("Pooh Bear", "Tigger")
+puts disney_characters.size
